ceee21d7445e59eca46e38b76ba3e919a37f2737
Tarin <tarinlove33@gmail.com>
2025-09-13 15:34:48 +1200

    EventInput DTOS made

--- Dtos/EventInput.cs
+++ Dtos/EventInput.cs
@@ -1,1 +1,18 @@
-﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace A2Template.Dtos
+{
+    public class EventInput
+    {
+        [Required]
+        public string Start { get; set; }
+        [Required]
+        public string End { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
