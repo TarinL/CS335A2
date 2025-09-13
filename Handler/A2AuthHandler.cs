5c1c36ce8dcb302093e18bce2f8a4160dafbb005
Tarin <tarinlove33@gmail.com>
2025-09-13 13:34:45 +1200

    AuthHandler made

--- Handler/A2AuthHandler.cs
+++ Handler/A2AuthHandler.cs
@@ -1,1 +1,85 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using A2Template.Data;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Security.Claims;
+
+namespace A2Template.Handler
+{
+    public class A2AuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly IA2Repo _repository;
+
+        public A2AuthHandler(
+            IA2Repo repository,
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder
+        ) : base(options, logger, encoder)
+        {
+            _repository = repository;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                Response.Headers.Add("WWW-Authenticate", "Basic");
+                return AuthenticateResult.Fail("Authorization header not found.");
+            }
+            else
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var cridentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var cridentials = Encoding.UTF8.GetString(cridentialBytes).Split(':');
+                var username =  cridentials[0];
+                var password = cridentials[1];
+
+                if (_repository.IsValidUser(username, password) && _repository.isValidStaff(username, password))
+                {
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, username), 
+                        new Claim(ClaimTypes.Role, "User"),
+                        new Claim(ClaimTypes.Role, "Staff")
+                    };
+
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, "Basic");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                    
+                    AuthenticationTicket ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                } 
+                else if (_repository.isValidStaff(username, password))
+                {
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, username),
+                        new Claim(ClaimTypes.Role, "Staff"),
+                    };
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, "Basic");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                    
+                    AuthenticationTicket ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                }
+                else if (_repository.IsValidUser(username, password))
+                {
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, username),
+                        new Claim(ClaimTypes.Role, "User"),
+                    };
+                }
+                return AuthenticateResult.Fail("Invalid username or password.");
+            }
+        }
+    }
+}
