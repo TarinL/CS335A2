fd9de30bd5eabf3095587031454de02606cd9858
Tarin <tarinlove33@gmail.com>
2025-09-14 23:37:29 +1200

    Endpoint 4 finsihed. Add event

--- Controllers/A2Controller.cs
+++ Controllers/A2Controller.cs
@@ -1,10 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 using A2Template.Models;
 using A2Template.Data;
 using A2Template.Dtos;
@@ -51,6 +52,39 @@ namespace A2Template.Controllers
         }
         
         // POST /webapi/AddEvent
+        [Authorize(AuthenticationSchemes = "Authentication")]
+        [Authorize(Policy = "StaffOnly")]
+        [HttpPost("AddEvent")]
+        public ActionResult<string> AddEvent(EventInput eventInput)
+        {
+            // check for correct date format
+            // yyyyMMddTHHmmssZ
+            string pattern = @"^\d{4}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])T([01]\d|2[0-3])[0-5]\d[0-5]\dZ$";
+            if (!Regex.IsMatch(eventInput.Start, pattern) && !Regex.IsMatch(eventInput.End, pattern))
+            {
+                return BadRequest("The format of Start and End should be yyyyMMddTHHmmssZ.");
+            }
+            else if (!Regex.IsMatch(eventInput.Start, pattern))
+            {
+                return BadRequest("The format of Start should be yyyyMMddTHHmmssZ.");
+            }
+            else if (!Regex.IsMatch(eventInput.End, pattern))
+            {
+                return BadRequest("The format of End should be yyyyMMddTHHmmssZ.");
+            }
+            
+            Event e = new Event
+            {
+                Description = eventInput.Description,
+                Location = eventInput.Location,
+                Summary = eventInput.Summary,
+                Start = eventInput.Start,
+                End = eventInput.End
+            };
+            _repository.AddEvent(e);
+            return Ok("Success");
+        }
+        
         // GET /webapi/EventCount
         [Authorize(AuthenticationSchemes = "Authentication")]
         [Authorize(Policy = "StaffOnly")]
@@ -60,6 +94,5 @@ namespace A2Template.Controllers
             int numOfEvents = _repository.GetAllEvents().Count();
             return Ok(numOfEvents);
         }
-        
     }
 }
